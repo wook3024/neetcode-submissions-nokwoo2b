f36aade0f0a4ad7d266a744fabbb633117710c83
Shinuk Yi <47630850+wook3024@users.noreply.github.com>
2026-04-07 22:26:55 +0900

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        parts = []
+        for str1 in strs:
+            parts.append(f"{len(str1)};{str1}")
+        return "".join(parts)
+
+
+    def decode(self, s: str) -> List[str]:
+        response = []
+        max_len = len(s)
+        cur_idx = 0 
+
+        print(s)
+        while cur_idx < max_len:
+            sep = s.find(";", cur_idx)
+            length = int(s[cur_idx:sep])
+            
+            start = sep + 1
+            end = start + length
+            response.append(s[start:end])
+            cur_idx = end
+            print(cur_idx)
+
+        return response
